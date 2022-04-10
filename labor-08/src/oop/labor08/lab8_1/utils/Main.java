3e4a3c03cc12fa85397b7e5552c8f011d4d8c0ff
SikoSzabolcs17 <sikoszabolcs17@yahoo.com>
2022-04-10 19:33:19 +0300

    Labor 8

--- labor-08/src/oop/labor08/lab8_1/utils/Main.java
+++ labor-08/src/oop/labor08/lab8_1/utils/Main.java
@@ -24,10 +24,45 @@ public class Main {
             bank.getCustomer(i).addBankAccount(new CheckingAccount(random.nextInt(100,1000)));
             bank.getCustomer(i).addBankAccount(new SavingsAccount(0.02));
         }
+
+        for (int i = 1; i <= bank.numCustomers(); i++) {
+            for (int j = 0; j < bank.getCustomer(i).getNumAccounts(); j++) {
+                bank.getCustomer(i).getBankAccountWithIndex(j).deposit(random.nextInt(100,1500));
+            }
+        }
+
         for (int i = 1; i <= bank.numCustomers(); i++) {
             System.out.println(bank.getCustomer(i));
         }
 
+
+        for (int i = 1; i <= bank.numCustomers(); i++) {
+            for (int j = 0; j < bank.getCustomer(i).getNumAccounts(); j++) {
+                if(bank.getCustomer(i).getBankAccountWithIndex(j) instanceof SavingsAccount){
+                    ((SavingsAccount) bank.getCustomer(i).getBankAccountWithIndex(j)).addIntrest();
+                }
+            }
+        }
+        for (int i = 1; i <= bank.numCustomers(); i++) {
+            System.out.println(bank.getCustomer(i));
+        }
+
+
+        for (int i = 1; i <= bank.numCustomers(); i++) {
+            for (int j = 0; j < bank.getCustomer(i).getNumAccounts(); j++) {
+
+                double value = random.nextInt(1000);
+
+                if(bank.getCustomer(i).getBankAccountWithIndex(j).withdraw(value)){
+                    System.out.println("Sikeres tranzakcio!" + "- " + value +" lej atutalva " + bank.getCustomer(i).getBankAccountWithIndex(j).getAccountNumber() + " szamlarol");
+                }
+                else {
+                    System.out.println("Sikertelen tranzakcio! - " +  value +" nincs ennyi forras " + bank.getCustomer(i).getBankAccountWithIndex(j).getAccountNumber() + " szamlan") ;
+                }
+
+            }
+        }
+        System.out.println();
         for (int i = 1; i <= bank.numCustomers(); i++) {
             System.out.println(bank.getCustomer(i));
         }
